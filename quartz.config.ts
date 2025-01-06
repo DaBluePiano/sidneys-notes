22e9cbf9ee7c9686c3ddfdee769c7b7d9e720dd3
Sidney Lam <sidney.lam@education.nsw.gov.au>
2025-01-06 17:45:39 +1100

    Quartz sync: Jan 6, 2025, 5:45 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🪴Sidney's Obsidian Notes :)",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -24,8 +24,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "Lexend",
+        body: "Lexend",
         code: "IBM Plex Mono",
       },
       colors: {
