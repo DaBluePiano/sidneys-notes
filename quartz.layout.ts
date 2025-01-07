2890af52fce1a34d73380e150a4003839f14551d
Sidney Lam <sidney.lam@education.nsw.gov.au>
2025-01-07 16:13:39 +1100

    Quartz sync: Jan 7, 2025, 4:13 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -37,8 +37,8 @@ export const defaultContentPageLayout: PageLayout = {
         depth: 1, // how many hops of notes to display
         scale: 1.1, // default view scale
         repelForce: 20, // how much nodes should repel each other
-        centerForce: 1, // how much force to use when trying to center the nodes
-        linkDistance: 30, // how long should the links be by default?
+        centerForce: 100, // how much force to use when trying to center the nodes
+        linkDistance: 3, // how long should the links be by default?
         fontSize: 0.6, // what size should the node labels be?
         opacityScale: 1, // how quickly do we fade out the labels when zooming out?
         removeTags: [], // what tags to remove from the graph
@@ -50,8 +50,8 @@ export const defaultContentPageLayout: PageLayout = {
         depth: -1,
         scale: 0.9,
         repelForce: 20,
-        centerForce: 1,
-        linkDistance: 30,
+        centerForce: 100,
+        linkDistance: 3,
         fontSize: 0.6,
         opacityScale: 1,
         removeTags: [], // what tags to remove from the graph
