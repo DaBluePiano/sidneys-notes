f34c95d9de12919fbc62eaf2d298b8c54f861d19
Sidney Lam <sidney.lam@education.nsw.gov.au>
2025-01-07 12:39:03 +1100

    Quartz sync: Jan 7, 2025, 12:39 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,7 +24,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "DM Sans",
+        header: "dmSans",
         body: "lexend",
         code: "jetbrains-mono",
       },
